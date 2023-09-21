233abbf6be13192c4211fe8c2237ea9e1d776a62
hafizullah.ozgur <hafizullah.ozgur@verisoft.com>
2023-09-21 11:34:36 +0300

    first commit

---
+++ clean-architecture-framework-4-8/Service/Global.asax.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using WebApi.StructureMap;
+using StructureMap.Configuration.DSL;
+using StructureMap.Graph;
+
+namespace CleanArchitecture.Service
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes); 
+            
+            GlobalConfiguration.Configuration.UseStructureMap(
+                p => p.Scan(
+                    scan => {
+                        scan.TheCallingAssembly();
+
+                        scan.WithDefaultConventions();
+
+                        scan.AssembliesFromApplicationBaseDirectory(
+                            filter => filter.FullName.StartsWith("CleanArchitecture"));                        
+                }));
+        }
+    }
+}
---
+++ clean-architecture-framework-4-8/Specification/Common/IoC.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Common.Dates;
+using StructureMap;
+using StructureMap.Graph;
+
+namespace CleanArchitecture.Specification.Common
+{
+    public static class IoC
+    {
+        public static IContainer Initialize(AppContext appContext)
+        {
+            var container = new Container(x =>
+            {
+                x.Scan(scan =>
+                {
+                    scan.AssembliesFromApplicationBaseDirectory(
+                        filter => filter.FullName.StartsWith("CleanArchitecture"));
+
+                    scan.WithDefaultConventions();
+                });
+
+                x.For<IDatabaseService>()
+                    .Use(appContext.DatabaseService);
+
+                x.For<IInventoryService>()
+                    .Use(appContext.InventoryService);
+
+                x.For<IDateService>()
+                    .Use(appContext.DateService);
+            });
+
+            return container;
+        }
+    }
+}
